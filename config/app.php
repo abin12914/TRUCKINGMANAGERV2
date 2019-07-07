2cd1cb1253cd23f107ea54817db7e62632177e11
abin12914 <abin111manjapra@gmail.com>
2019-07-08 00:04:48 +0530

    awesome landlord added

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //awesome landlord
+        Torzer\Awesome\Landlord\LandlordServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        //awesome landlord
+        'Landlord' => Torzer\Awesome\Landlord\Facades\Landlord::class,
 
     ],
 
